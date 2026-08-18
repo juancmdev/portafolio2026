33d402a7b9b2e32f74d43c57a62a208dd12ca3da
juancmdev <draxustienda@gmail.com>
2026-08-18 17:37:50 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-4xl'>Portafolio 2026</h1>
+    </>
+  )
+}
+
+export default App
